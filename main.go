37171ac37fb91b851e266e7c7c55a6dd420b2159
Christopher Johnstone <chrisj97@proton.me>
2026-07-01 20:40:12 +0100

    feat: Normalised feed

--- main.go
+++ main.go
@@ -5,13 +5,30 @@ import (
 	"log"
 )
 
+type Feed struct {
+	Title       string
+	Description string
+	Link        string
+	FeedType    string // "rss" or "atom"
+	Items       []Item
+}
+
+type Item struct {
+	Title       string
+	Description string
+	Link        string
+	Author      string
+	PubDate     string // could use time.Time later
+	ID          string // RSS: guid, Atom: id
+}
+
 func main() {
 	var url string = "https://feeds.bbci.co.uk/news/rss.xml"
-	rss, err := rssFeed(url)
+	feed, err := fetchRSSFeed(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, item := range rss.Channel.Items {
+	for _, item := range feed.Items {
 		fmt.Println(item.Title)
 	}
 }
--- rss.go
+++ rss.go
@@ -7,37 +7,56 @@ import (
 	"net/http"
 )
 
-type RSS struct {
+type rssFeed struct {
 	Channel struct {
-		Title       string `xml:"title"`
-		Link        string `xml:"link"`
-		Description string `xml:"description"`
-		Items       []struct {
-			Title       string `xml:"title"`
-			Link        string `xml:"link"`
-			Description string `xml:"description"`
-			PubDate     string `xml:"pubDate"`
-			Author      string `xml:"author"`
-		} `xml:"item"`
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Items       []rssItem `xml:"item"`
 	} `xml:"channel"`
 }
 
-func rssFeed(url string) (RSS, error) {
+type rssItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+	Author      string `xml:"author"`
+	GUID        string `xml:"guid"`
+}
+
+func fetchRSSFeed(url string) (Feed, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return RSS{}, err
+		return Feed{}, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return RSS{}, fmt.Errorf("bad status: %d", resp.StatusCode)
+		return Feed{}, fmt.Errorf("bad status: %d", resp.StatusCode)
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return RSS{}, err
+		return Feed{}, err
 	}
-	var rss RSS
+	var rss rssFeed
 	if err := xml.Unmarshal(body, &rss); err != nil {
-		return RSS{}, err
+		return Feed{}, err
 	}
-	return rss, nil
+	feed := Feed{
+		Title:       rss.Channel.Title,
+		Description: rss.Channel.Description,
+		Link:        rss.Channel.Link,
+		FeedType:    "rss",
+	}
+	for _, item := range rss.Channel.Items {
+		feed.Items = append(feed.Items, Item{
+			Title:       item.Title,
+			Description: item.Description,
+			Link:        item.Link,
+			PubDate:     item.PubDate,
+			Author:      item.Author,
+			ID:          item.GUID,
+		})
+	}
+	return feed, nil
 }
